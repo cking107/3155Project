be9edb7df9e7694fdc567d2987014ee10689e859
John Rockhead <jrockhea@uncc.edu>
2018-12-04 03:49:18 +0000

    completed recipe adder shows a recipe test

--- spec/integration/recipeadder_shows_a_recipe_spec.rb
+++ spec/integration/recipeadder_shows_a_recipe_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper.rb'
 
-feature "Recipe adder deletes a recipe" do
-    scenario "Recipe adder successfully deletes a recipe from the list" do
+feature "Recipe adder shows a recipe" do
+    scenario "Recipe adder successfully shows a recipe from the list" do
         visit new_article_path
         expect(page).to have_content("New Recipe")
         fill_in "Meal", with: "New Capybara Recipe"
@@ -12,7 +12,13 @@ feature "Recipe adder deletes a recipe" do
         click_link "Recipe List"
         expect(page).to have_content("New Capybara Recipe")
         expect(page).to have_content("This is a new Capybara Ingredient")
-        click_link "Delete"
-        click_button "OK"
+        click_link "Show"
+        expect(page).to have_content("Your Recipe")
+        expect(page).to have_content("Meal: New Capybara Recipe")
+        expect(page).to have_content("Ingredients: This is a new Capybara Ingredient")
+        expect(page).to have_content("Instructions")
+        expect(page).to have_content("Add instructions:")
+        expect(page).to have_field("Author")
+        expect(page).to have_field("Instructions")
     end
 end
