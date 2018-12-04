faad69f470ef819a52a97e774ca137f273d7f91e
John Rockhead <jrockhea@uncc.edu>
2018-12-04 21:17:37 +0000

    finished imagemagick and adding image to the views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     has_many :comments, dependent: :destroy
-    has_attached_file :image, styles: { large: "600x600>", medium: "300x300>" , thumb: "100x100>"}, default_url: "/images/:style/missing.png"
+    has_attached_file :image, styles: { large: "600x600>", medium: "300x300>" , thumb: "150x150>"}
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
     
     validates :title, presence: true, length: { minimum: 5 }
